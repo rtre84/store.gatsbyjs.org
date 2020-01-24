ec6496b4d26b08b8913298e21c2f693f96a1169a
rtre84 <rtre84@users.noreply.github.com>
2020-01-24 09:10:59 -0500

    Changed 404 page to not include gatsby store links.

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,20 +13,7 @@ const NotFoundPage = () => (
       <Heading>Whoops - That Page Doesn’t Exist (404)</Heading>
       <Text>
         Looks like the page you requested either doesn’t exist or has been
-        moved. If you think this is an error or ended up at this page by
-        following a link, please{' '}
-        <a href="https://github.com/gatsbyjs/gatsby/issues/new">
-          open an issue
-        </a>{' '}
-        to let us know.
-      </Text>
-      <Text>
-        {' '}
-        <a href="https://store.gatsbyjs.org/">Gatsby Store Home page</a>{' '}
-        <a href="https://www.gatsbyjs.org/">Gatsby Home</a>{' '}
-        <a href="https://github.com/gatsbyjs/store.gatsbyjs.org">
-          Gatsby Github Page
-        </a>{' '}
+        moved.
       </Text>
     </div>
   </TextContainer>
